747ed9991c49cb601f1eb06690fa0031565a698d
wizkidorg <5863936+wizkidorg@users.noreply.github.com>
2021-05-05 14:15:06 -0400

    werkzeug deprecated  functions
    fox for error -  from werkzeug import generate_password_hash, check_password_hash
    Those functions were deprecated for a while and now they are moved to werkzeug.security

--- ad2web/user/models.py
+++ ad2web/user/models.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy import Column, types
 from sqlalchemy.ext.mutable import Mutable
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 from ..extensions import db
